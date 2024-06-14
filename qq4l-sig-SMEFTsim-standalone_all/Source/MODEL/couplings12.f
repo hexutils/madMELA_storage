c6b6f6b9f26a8205ef23dbb2f853666fe3867b4a
jejeffre <jdavi231@jhu.edu>
2024-06-15 00:02:59 +0200

    qqH process

---
+++ qq4l-sig-SMEFTsim-standalone_all/Source/MODEL/couplings12.f
@@ -0,0 +1,32 @@
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c      written by the UFO converter
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      SUBROUTINE COUP12()
+
+      IMPLICIT NONE
+      INCLUDE 'model_functions.inc'
+
+      DOUBLE PRECISION PI, ZERO
+      PARAMETER  (PI=3.141592653589793D0)
+      PARAMETER  (ZERO=0D0)
+      INCLUDE 'input.inc'
+      INCLUDE 'coupl.inc'
+      GC_1382 = -((MDL_CLEQU1IM*MDL_YM*MDL_YUP)
+     $ /MDL_LAMBDASMEFT__EXP__2)
+      GC_1383 = (MDL_CLEQU1IM*MDL_YM*MDL_YUP)/MDL_LAMBDASMEFT__EXP__2
+      GC_1387 = -((MDL_CLEQU1RE*MDL_COMPLEXI*MDL_YM*MDL_YUP)
+     $ /MDL_LAMBDASMEFT__EXP__2)
+      GC_1391 = -(MDL_CLEQU3IM*MDL_YM*MDL_YUP)/(2.000000D+00
+     $ *MDL_LAMBDASMEFT__EXP__2)
+      GC_1392 = -(MDL_CLEQU3IM*MDL_YM*MDL_YUP)/(4.000000D+00
+     $ *MDL_LAMBDASMEFT__EXP__2)
+      GC_1393 = (MDL_CLEQU3IM*MDL_YM*MDL_YUP)/(4.000000D+00
+     $ *MDL_LAMBDASMEFT__EXP__2)
+      GC_1394 = (MDL_CLEQU3IM*MDL_YM*MDL_YUP)/(2.000000D+00
+     $ *MDL_LAMBDASMEFT__EXP__2)
+      GC_1401 = -(MDL_CLEQU3RE*MDL_COMPLEXI*MDL_YM*MDL_YUP)/(4.000000D
+     $ +00*MDL_LAMBDASMEFT__EXP__2)
+      GC_1402 = (MDL_CLEQU3RE*MDL_COMPLEXI*MDL_YM*MDL_YUP)/(2.000000D
+     $ +00*MDL_LAMBDASMEFT__EXP__2)
+      END
